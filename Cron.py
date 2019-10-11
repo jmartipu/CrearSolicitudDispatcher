c0772ce3fe002f9d161b6d2ccdfe481390bde0d3
Jorge Martín <jormartin77@gmail.com>
2019-10-11 18:35:11 -0500

    switch case de las urls

--- Cron.py
+++ Cron.py
@@ -4,8 +4,17 @@ import Settings
 from SQSConnection import SQSConnection
 from threading import Thread
 
-def dispatch(msg):
-    sqs_connection_out = SQSConnection(Settings.AWS_QUEUE_URL_OUT)
+
+def dispatch(msg, tipo):
+    if tipo == 'Cypress':
+        sqs_connection_out = SQSConnection(Settings.AWS_QUEUE_URL_OUT_CYPRESS)
+    elif tipo == 'Puppeteer':
+        sqs_connection_out = SQSConnection(Settings.AWS_QUEUE_URL_OUT_CYPRESS)
+    elif tipo == 'Adb':
+        sqs_connection_out = SQSConnection(Settings.AWS_QUEUE_URL_OUT_CYPRESS)
+    else:
+        sqs_connection_out = SQSConnection(Settings.AWS_QUEUE_URL_OUT_CYPRESS)
+
     print('Despachando')
     sqs_connection_out.send(msg)
     print('Despachado')
@@ -18,8 +27,9 @@ def process():
         with sqs_connection_in:
             sqs_connection_in.receive()
             if sqs_connection_in.message is not '':
+                message_tipo = sqs_connection_in.message.message_attributes.get('Tipo').get('StringValue')
                 message_body = sqs_connection_in.message.get('MessageBody')
-                dispatch(message_body)
+                dispatch(message_body, message_tipo)
                 sqs_connection_in.delete()
                 
     except Exception as e:
